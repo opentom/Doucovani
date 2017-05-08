1ae07bb57611fb7712f7b169b2dbe12587f21a78
Unknown <uschovnaspamu@seznam.cz>
2017-05-08 22:57:10 +0200

    Project start
    Start of the project.

---
+++ data_analysis.py
@@ -0,0 +1,10 @@
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+
+tmpvar = pd.read_csv('C:\\Users\\glabka\\Dropbox\\Apps\\Plain.txt\\doucovani.txt')
+revenue = tmpvar
+
+print(revenue)
+print('----------------------')
+print(revenue[0:2])
